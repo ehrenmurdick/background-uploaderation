e9fdb90f893a866ea164e80fa0c1d4dd3a17a675
Ehren Murdick <ehren@theedgecase.com>
2009-04-16 17:27:15 -0400

    file move instead of read/writ

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -3,8 +3,15 @@ class UploadsController < ApplicationController
   end
 
   def create
-    filename = "tmp/uploads/#{params[:Filename]}"
-    %x{cat #{params[:Filedata].path} > #{filename}}
+    filename = "tmp/uploads/#{Time.now.to_f}.#{params[:Filename]}"
+    if Tempfile === params[:Filedata]
+      FileUtils.mv(params[:Filedata].path, filename)
+    else
+      File.open(filename, 'w') do |f|
+        f.write(params[:Filedata].read)
+      end
+    end
     render :text => filename
+    GC.start
   end
 end
